c366c2fef95d4905109d991c5efceeb787f54156
root <tulkiidra@gmail.com>
2022-09-30 01:10:26 -0700

    Puts Sentence

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
 	/*Create string with character*/
-	char Programming[]="Programming is like building a multilingual puzzle";
+	char Programming[] = "\"Programming is like building a multilingual puzzle";
 
 	/*Write Programming*/
 	puts(Programming);
 
-	return 0;
+	return(0);
 
 
 }
