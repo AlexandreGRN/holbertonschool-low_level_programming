793cbe2c1206d2caf0e0405eb5e72d93fc730d57
root <tulkiidra@gmail.com>
2022-09-30 01:52:18 -0700

    Printf string

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*Create string with character*/
+	char Programming[] = "\"Programming is like building a multilingual puzzle";
+
+	/*Write Programming*/
+	puts(Programming);
+
+	return (0);
+}
