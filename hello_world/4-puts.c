ec4ed8aa761f33cf33a88cdd585d5b57a0667691
root <tulkiidra@gmail.com>
2022-09-30 01:27:17 -0700

    Puts Sentence

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,9 +6,9 @@ int main(void)
 	char Programming[] = "\"Programming is like building a multilingual puzzle";
 
 	/*Write Programming*/
-	puts (Programming);
+	puts(Programming);
 
-	return(0);
+	return (0);
 
 
 }
