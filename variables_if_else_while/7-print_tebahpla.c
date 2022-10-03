14856d551b7b4fefedd8f109caaab11ae8ebaa2a
root <tulkiidra@gmail.com>
2022-10-03 06:15:34 -0700

    Print numbers v2

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z' ; alpha <= 'a' ; alpha--)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+	return (0);
+}
