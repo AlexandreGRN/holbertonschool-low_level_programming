e560418a9774a6426037e6af2e15247c65b5c873
root <tulkiidra@gmail.com>
2022-10-03 07:19:05 -0700

    Print base 16 digit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha, num;
+
+	for (num = '0' ; num < '9' ; num++)
+	{
+		putchar(num);
+	}
+
+	for (alpha = 'a' ; alpha > 'f' ; alpha--)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
