5c60a9c4f7463556ad179fd855d1ef889902946e
root <tulkiidra@gmail.com>
2022-10-03 04:27:21 -0700

    Alphabet+Caps

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (alpha = 'A' ; alpha <= 'Z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
