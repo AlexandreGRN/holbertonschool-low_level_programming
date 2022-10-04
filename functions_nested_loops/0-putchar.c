ac2754683fe42b506fab623a55d367c1196341d9
root <tulkiidra@gmail.com>
2022-10-04 04:09:58 -0700

    Write _Putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "\_putchar\n",10);
+
+	return (0);
+}
