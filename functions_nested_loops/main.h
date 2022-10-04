5d6c67f01768241e204b04b459bde8c1e34ba104
Alexandre Guerin <113639808+Tulkiidra@users.noreply.github.com>
2022-10-04 19:08:11 +0200

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <unistd.h>
-
-/* function print _putchar */
-int _putchar(char);
-/* function print_alphabetic */
+int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
