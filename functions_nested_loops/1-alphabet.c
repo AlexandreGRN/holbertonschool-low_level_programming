62c5eff1c32b8f28f5bf567f8235f66070cb4c92
Alexandre Guerin <113639808+Tulkiidra@users.noreply.github.com>
2022-10-04 19:17:48 +0200

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
 
 void print_alphabet(void);
 {
-	char alpha
+	char alpha;
 	for (alpha='a', alpha <= 'z', alpha++)
 	{
-		_putchar('alpha');
+		_putchar(alpha);
 	}
-	_putchat(\n);
+	_putchat('\n');
 	return (0);
 }
