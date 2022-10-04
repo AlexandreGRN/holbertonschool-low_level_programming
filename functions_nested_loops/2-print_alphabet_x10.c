d1865842733f369e938d98862089274c82fd3cc9
root <tulkiidra@gmail.com>
2022-10-04 06:02:33 -0700

    alphabet*10 function

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char alpha, num
+	for (num = 0, num !=10, num++)
+	{
+		for (alpha='a', alpha <= 'z', alpha++)
+		{
+			_putchar('alpha');
+		}
+	_putchat(\n);
+	}
+	return (0);
+}
