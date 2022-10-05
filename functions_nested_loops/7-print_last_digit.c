390bd249a68f5caf35ac37eb540009b3afbd4258
root <tulkiidra@gmail.com>
2022-10-05 00:41:29 -0700

    Changing commentary

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_last_digit - print the last digit of the number
+ * print_last_digit - print the last digit of the number
  *@n: character to check
- *Return the value of the last digit
+ *Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
 int m;
 
-m=n%10
+m = n % 10;
 return (m);
 }
