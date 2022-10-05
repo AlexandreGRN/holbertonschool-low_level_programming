fb1d11cc370b2ff553cbd217b56a82e3467c3fb3
root <tulkiidra@gmail.com>
2022-10-05 14:07:39 -0700

    add readme, task 0, main.h

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+include "main.h"
+include < stdio.h >
+
+
+/**
+ *_isupper - Check for uppercase chara
+ * @c : character we will test
+ * Return 1 if uppercase, 0 if not
+ */
+
+int _isupper(int c);
+{
+	if (c >= '65' && c <= '90')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
