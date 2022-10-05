c50b8f634399d0f1db24e618737f41e998537dcb
root <tulkiidra@gmail.com>
2022-10-05 00:29:43 -0700

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_sign - print the absolute value of the number
+ *@n: character to check
+ *Return: n if n  > 0 | return -n if n < 0
+ */
+int print_sign(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
