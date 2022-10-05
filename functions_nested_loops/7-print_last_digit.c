758dad8018688d013b963055cfac7370c6fc92c5
root <tulkiidra@gmail.com>
2022-10-05 02:29:00 -0700

    addition

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,19 @@ int print_last_digit(int n)
 {
 int u;
 
-if (n > 0)
+if (n < 0)
 {
+n = -n;
+}
 u = n % 10;
-_putchar(u + '0');
+if (u == '(')
+{
+_putchar(8);
+return (8);
 }
 else
 {
-n = -n;
-u = n % 10;
 _putchar(u + '0');
-}
 return (u);
+}
 }
