9af1debd6a03a651f4e9a566cf8446303a163642
root <tulkiidra@gmail.com>
2022-10-05 01:25:10 -0700

    times tables

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *jack_bauer - print every minutes in a day
+ *
+ *Return: void
+ */
+void jack_bauer(void)
+{
+int n, m;
+
+for (n = 0; n < 10; n++)
+{
+for (m = 0; min < 10; m++)
+{
+print("0")
+res = n * m;
+if ((res) < 10)
+{
+printf(",  %d", res);
+}
+else
+{
+printf(", %d", res);
+}
+putchar('\n')
+}
+}
+}
