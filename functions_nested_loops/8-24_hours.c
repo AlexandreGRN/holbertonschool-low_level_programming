dfcc153a76d9844f06c08844bc8aa66a6f1218e4
root <tulkiidra@gmail.com>
2022-10-05 01:04:32 -0700

    adding 00:00 syntax

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,7 +13,23 @@ for (hour = 0; hour < 24; ++hour)
 {
 for (min = 0; min < 60; ++min)
 {
+if (hour < 10)
+{
+if (min < 10)
+{
+printf("0%d:0%d", hour, min);
+}
+else
+{
+printf("0%d:%d", hour, min);
+}
+else if (min < 10)
+printf("%d:0%d", hour, min);
+else
+{
 printf("%d:%d", hour, min);
 }
+}
+_putchar('\n');
 }
 }
