37342df04d18a734dba22b46c0b537e9e1b89462
root <tulkiidra@gmail.com>
2022-10-05 02:55:09 -0700

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,27 +2,26 @@
 #include "main.h"
 /**
  * print_to_98 - print every number between n and 98
- * @a: character 1
- * @b: character 2 added together
- *Return: int
+ * @n: base number
+ *Return: void
  */
 void print_to_98(int n)
 {
 if (n > 98)
 {
-  _putchar(n);
-  for (n ; n > 98 ; --n)
-  {
-    printf(", %d", n);
-  }
+_putchar(n);
+for (n ; n > 98 ; --n)
+{
+printf(", %d", n);
+}
 }
 else if (n < 98)
 {
-  _putchar(n);
-  for (n ; n < 98 ; ++n)
-  {
-    printf(", %d", n);
-  }
+_putchar(n);
+for (n ; n < 98 ; ++n)
+{
+printf(", %d", n);
+}
 }
 return (0);
 }
