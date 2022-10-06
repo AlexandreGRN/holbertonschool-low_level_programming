6f1b50c1bb617a744247a278172a524007e5e295
root <tulkiidra@gmail.com>
2022-10-06 11:38:44 -0700

    testFiz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print number between 1 - 100, and multiples of 3 = Fizz
+ * Multiples of 5 = Buzz, and both = FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 100 ; n++)
+	{
+		/*if multiples of 3 and 5 FizzBuzz*/
+		if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz");
+			_putchar(' ');
+		}
+		/*else, if only multiples of 3 Fizz*/
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz");
+			_putchar(' ');
+		}
+		/*else, if only multiples of 5 Buzz */
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz");
+			_putchar(' ');
+		}
+		/*if neither, print n*/
+		else
+		{
+			printf("%d", n);
+			_putchar(' ');
+		}
+	}
+	printf("Buzz");
+	return (0);
+}
