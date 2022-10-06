7b7a40d26ff39cd9c236775d9a64a65acc544b5c
root <tulkiidra@gmail.com>
2022-10-06 06:31:18 -0700

    la

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print all 10 digits
+ * Return: result
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		_putchar(n);
+	}
+}
