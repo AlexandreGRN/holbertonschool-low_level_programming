126c09e423d17c365a10383353185f7566f0da4e
root <tulkiidra@gmail.com>
2022-10-06 07:37:25 -0700

    5bug

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print all 10 digits
+ * Return: result
+ */
+void more_numbers(void)
+{
+	int n = 48;
+	int m;
+	int dizaine = 0;
+
+	for (m = 0 ; m <= 14 ; m++)
+	{
+		if (dizaine = 1)
+		{
+			_putchar(49);
+		}
+		_putchar(n);
+		if (n >= 57)
+		{
+			n = 48;
+			dizaine = 1;
+		}
+		
+	}
+	_putchar('\n');
+}
