f063f284073833f20f975770f0e0a4725ab2bb3f
root <tulkiidra@gmail.com>
2022-10-06 06:13:15 -0700

    Test if is a digit

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #include <unistd.h>
 #include <stdarg.h>
 int _isupper(int c);
-/*int _isdigit(int c);
-int mul(int a, int b);
+int _isdigit(int c);
+/*int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void)
