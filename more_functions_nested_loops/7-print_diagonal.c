5828904739390124217324b09a58df0bee7b30bb
root <tulkiidra@gmail.com>
2022-10-06 10:59:26 -0700

    Change symbol to backslash

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * @n : number of line
+ * Return: result
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+	if (n > 0)
+	{
+		/*boucle n lignes*/
+		for (a = 0 ; a < n; a++)
+		{
+			/*mets 1 espace par nombre de ligne*/
+			b = a;
+			while(b > 0)
+			{
+				_putchar(' ');
+				b--;
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+
+}
