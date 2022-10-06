280aedf2050ffc3b6ebce511d101acd2ed2d79e8
root <tulkiidra@gmail.com>
2022-10-06 06:18:42 -0700

    Mult

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #include <stdarg.h>
 int _isupper(int c);
 int _isdigit(int c);
-/*int mul(int a, int b);
-void print_numbers(void);
+int mul(int a, int b);
+/*void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void)
 void print_line(int n);
