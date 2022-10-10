3875b73654a31ae6c9fd26994645f70a61ff9256
root <tulkiidra@gmail.com>
2022-10-10 09:21:36 -0700

    Changing Half value for odd length

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,18 +9,18 @@ void puts_half(char *str)
 {
 	int half;
 	int lgt = 0;
-	
+
 	while (str[lgt] != '\0') /*Length of string*/
 	{
 		lgt++;
 	}
-	if (lgt % 2 == 0 )
+	if (lgt % 2 == 0)
 	{
 		half = lgt / 2;
 	}
 	else
 	{
-		half = (lgt - 1) / 2;
+		half = (lgt + 1) / 2;
 	}
 	for (half = half ; str[half] != '\0' ; half++)
 	{
