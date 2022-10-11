1f841a358b22146a6f29e6369f1c3272cdd6db65
root <tulkiidra@gmail.com>
2022-10-11 10:26:07 -0700

    Betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,16 @@ int _strcmp(char *s1, char *s2)
 {
 	 int i = 0;
 
-	 while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != '\0' && s2[i] == '\0')
 			return (s1[i]);
 		else if (s2[i] != '\0' && s1[i] == '\0')
 			return (-s2[i]);
+		else if (s1[i] != s2[i])
+				return (s1[i]i - s2[i]);
 		else
-		{
-			if (s1[i] != s2[i])
-				return (s1[i]-s2[i]);
-			else
-				i++;
-		}
+			i++;
 	}
-	 return (0);
+	return (0);
 }
