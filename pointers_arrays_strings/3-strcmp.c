2b4542b24c9c10d10c3ac35f32ac0496908ada03
root <tulkiidra@gmail.com>
2022-10-11 10:28:00 -0700

    Betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,13 +14,21 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != '\0' && s2[i] == '\0')
+		{
 			return (s1[i]);
+		}
 		else if (s2[i] != '\0' && s1[i] == '\0')
+		{
 			return (-s2[i]);
+		}
 		else if (s1[i] != s2[i])
-				return (s1[i] - s2[i]);
+		{
+			return (s1[i] - s2[i]);
+		}
 		else
+		{
 			i++;
+		}
 	}
 	return (0);
 }
