1a7e8c0ef7a673fe5ffa4a62cd594ec5d0bd5d79
root <tulkiidra@gmail.com>
2022-10-12 06:03:05 -0700

    Uploading function memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char *_memcpy - Copy memory area
+ * @dest: where to copy
+ * @src: what to copy
+ * @n: number of byte copied
+ * Return: adress to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int p;
+
+	for (p = 0 ; p != n ; p++)
+	{
+		dest[p] = src[p];
+	}
+	return (dest);
+}
