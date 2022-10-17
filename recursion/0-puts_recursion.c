488f9206c8862a220151cfbc458ee4e78efdaad8
root <tulkiidra@gmail.com>
2022-10-17 15:14:17 -0700

    Upload All 7 tasks

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,8 +4,16 @@
  * _puts_recursion - print a string + newline
  * @s: string to print
  */
-
 void _puts_recursion(char *s)
+{
+	recursion(s);
+	_putchar('\n');
+}
+/**
+ * recursion - print a string
+ * @s: string to print
+ */
+void recursion(char *s)
 {
 	if (*s != '\0')
 	{
