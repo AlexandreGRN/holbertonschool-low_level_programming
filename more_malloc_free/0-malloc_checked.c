1f154345dc93d9507a9481713b44cae1575da0ed
root <tulkiidra@gmail.com>
2022-10-25 06:11:50 -0700

    malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,11 +3,15 @@
 /**
  * malloc_checked - allocate memory
  * @b: size allocated
+ * Return: ptr to space
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-
+	ptr = malloc(b);
+	if (b == 0)
+		return (98);
+	return (ptr);
 }
