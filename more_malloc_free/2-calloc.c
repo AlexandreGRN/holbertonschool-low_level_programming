0099b4a716fd84617fd366a8e6670c0b17106998
root <tulkiidra@gmail.com>
2022-10-25 07:56:21 -0700

    prout

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (0);
 	}
 
-	for (i = 0 ; i != nmemb ; i++) /*memory set to 0*/
+	for (i = 0 ; i != nmemb * size ; i++) /*memory set to 0*/
 		ptr[i] = 0;
 
 	return (ptr);
