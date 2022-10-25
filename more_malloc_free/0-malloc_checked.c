0f7c1494dfac563f9134277e3ab1d114ed2c65a7
root <tulkiidra@gmail.com>
2022-10-25 05:24:39 -0700

    String concat

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory
+ * @b: size allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+
+}
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenante s1 + n bit of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bite
+ * Return: pointer to the new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len1, len2, i, j;
+	char *new;
+
+	if (s2 == NULL)
+		*s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n > len2)
+		n = len2;
+
+	new = malloc(len1 + n + 1);
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+		new[i] = s1[i];
+	}
+
+	for (j = 0 ; j < n ; j++)
+	{
+		new[i] = s2[j];
+		i++;
+	}
+
+	new[i] = '\0';
+	return (new);
+}
