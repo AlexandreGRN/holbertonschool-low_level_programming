92b72d13fa057ef1bcfc1b1c654ead6c368687df
root <tulkiidra@gmail.com>
2022-10-25 03:24:14 -0700

    Updload free

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
+ * free_grid - free the precedent grid
+ * @grid: said grid
+ * @height: height of the grid
  */
+
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = 0 ; i != height ; i++)
+		free(grid[i]);
+	free(**grid);
+}
