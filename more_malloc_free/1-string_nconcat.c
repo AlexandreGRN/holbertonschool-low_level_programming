4ed876332c7d7470fea101dcce34c38020972139
root <tulkiidra@gmail.com>
2022-10-25 05:37:27 -0700

    String concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,28 +13,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int len1, len2, i, j;
 	char *new;
 
-	if (s2 == NULL)
-		*s2 = "";
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
+	if (s1 == NULL && s2 == NULL)
+	{
+		len1 = 0;
+		len2 = 0;
+	}
+	else if (s1 == NULL)
+	{
+		len1 = 0;
+		len2 = strlen(s2);
+	}
+	else if (s2 == NULL)
+	{
+		len1 = strlen(s1);
+		len2 = 0;
+	}
+	else
+	{
+		len1 = strlen(s1);
+		len2 = strlen(s2);
+	}
 	if (n > len2)
 		n = len2;
 
 	new = malloc(len1 + n + 1);
-
 	for (i = 0 ; s1[i] != '\0' ; i++)
 	{
 		new[i] = s1[i];
 	}
-
 	for (j = 0 ; j < n ; j++)
 	{
 		new[i] = s2[j];
 		i++;
 	}
-
 	new[i] = '\0';
 	return (new);
 }
