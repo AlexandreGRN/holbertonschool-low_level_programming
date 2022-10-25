c2c7a6b652e9522ce17a5549c96f5b5ab3232cf8
root <tulkiidra@gmail.com>
2022-10-25 05:54:10 -0700

    More test for NULL malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = len2;
 	new = malloc(len1 + n + 1);
 	if (new == 0)
+	{
 		free(new);
+		return (0);
+	}
 	for (i = 0 ; i != len1 ; i++)
 		new[i] = s1[i];
 	for (j = 0 ; j < n ; j++, i++)
