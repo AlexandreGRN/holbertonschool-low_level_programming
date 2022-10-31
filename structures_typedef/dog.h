d090f07ad948157848f06a42922a8a570ef005c8
root <tulkiidra@gmail.com>
2022-10-31 02:47:39 -0700

    README + dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - infos about dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: name, age and owner name of a dog
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+#endif
