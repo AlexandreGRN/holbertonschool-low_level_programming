aba3e635817a704d1d72c320374578b36ded6e0b
root <tulkiidra@gmail.com>
2022-11-01 04:58:55 -0700

    Test if name is NULL

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - prints a name
  * @name: name
@@ -8,5 +8,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name)
 	(*f)(name);
 }
