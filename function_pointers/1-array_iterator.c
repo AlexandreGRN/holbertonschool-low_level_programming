0d32d3a0416ed84a6f652f39e7fca625b37d29e9
root <tulkiidra@gmail.com>
2022-11-01 04:54:52 -0700

    Adding more particuliar cases

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include  "function_pointers.h"
+#include <stdlib.h>
 /**
  * array_iterator - exectute a function on each elem of an array
  * @array: the array
@@ -11,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	long unsigned int i;
 
 	if (array == NULL || action == NULL)
-		exit();
+		exit("");
 	for (i = 0 ; i != size ; i++)
 	{
 		(*action)(array[i]);
