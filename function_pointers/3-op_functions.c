39ed803530443f2f8f7b369298b84f08efd7ed36
root <tulkiidra@gmail.com>
2022-11-02 04:20:21 -0700

    Calculator

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add 2 int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub 2 int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mul 2 int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div 2 int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod 2 int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
