f6c6b162aaa701e10800ec9c7f2ce3739256b573
root <tulkiidra@gmail.com>
2022-11-03 05:07:37 -0700

    Adding Ascii test

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -30,10 +30,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			}
 		}
 		if (i < n - 1)
+		{
 			if (separator && *separator != 32)
 				printf("%c ", *separator);
 			else if (*separator == 32)
 				printf(" ");
+		}
 	}
 	printf("\n");
 }
