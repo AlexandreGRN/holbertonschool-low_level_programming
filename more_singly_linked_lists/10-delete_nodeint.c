3d5222c276ead0d683ab93ec98793b070f2e4e59
root <tulkiidra@gmail.com>
2022-11-15 07:55:36 -0800

    uploading task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete a certain node
+ * @head: head of the list
+ * @index: index of the node suppressed
+ * Return: 1 or -1;
+ */
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *copyHead; /*for index > 0*/
+	listint_t **copyNext;
+	unsigned int count = 1;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	copyHead = *head; /*store head in copyHead*/
+	if (index > 0)
+	{
+		while (count < index) /*travel every node before index - 1*/
+		{
+			if ((*head) == NULL)
+				return (-1);
+			count++;
+			(*head) = (*head)->next;
+		}
+		copyNext = &((*head)->next); /*link the adress of (*head).next*/
+		(*head) = (*head)->next;
+		*copyNext = ((*head)->next); /*change the value of (*head)->next) */
+		free((*head));
+		*head = copyHead;
+		return (1);
+	}
+	else
+	{
+		copyHead = (*head)->next;/*store (*head)->next to make next as new head*/
+		free(*head);
+		(*head) = copyHead;
+		return (1);
+	}
+}
