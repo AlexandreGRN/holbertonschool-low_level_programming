d63505cf65b0c4f080f2be7349d75335ab360de2
root <tulkiidra@gmail.com>
2022-11-16 04:58:53 -0800

    Uploading task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index -  insert a node at a certain place
  * @head: first node of the list
@@ -15,8 +14,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t **copyPreviousNode = NULL;
 	unsigned int counter = 0;
 
+	if (idx == 0)
+	{
+		newNode->next = (*head);
+		(*head) = newNode;
+	}
 	/*creating node to insert*/
 	newNode = malloc(sizeof(listint_t));
+	if (newNode == 0)
+		return (0);
 	newNode->n = n;
 	copyHead = (*head);/*store (*head) */
 	copyPreviousNode = &((*head)->next);
