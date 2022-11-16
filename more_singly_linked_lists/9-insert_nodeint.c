0b3f03713f86021b1ee85b8616d7e2324ff7015b
root <tulkiidra@gmail.com>
2022-11-16 09:10:46 -0800

    Rework idk try

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,43 @@
 #include "lists.h"
 /**
  * insert_nodeint_at_index -  insert a node at a certain place
- * @head: first node of the list
- * @idx: idx where the node will be added
- * @n: int to add in node
+ * @head: head of the list
+ * @idx: idx where the node will be insert
+ * @n: int in node
  * Return: the adress of the new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *copyHead = NULL; /*store (*head)*/
-	listint_t *newNode = NULL; /*new node created*/
-	listint_t **copyPreviousNode = NULL;
-	unsigned int counter = 0;
+	listint_t *node;
+	listint_t *newNode;
+	unsigned int count = 0; /*counter*/
 
-	/*creating node to insert*/
 	newNode = malloc(sizeof(listint_t));
-	if (newNode == 0)
-		return (0);
+	if (head == NULL || newNode == NULL)
+		return (NULL);
+
 	newNode->n = n;
-	copyHead = (*head);/*store (*head) */
-	copyPreviousNode = &((*head)->next);
+	newNode->next = NULL;
 	if (idx == 0)
 	{
-		newNode->next = (*head);
-		(*head) = newNode;
-		return ((*head));
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
-	while (counter <= idx)
+	node = (*head);
+
+	while (node)
 	{
-		if (*head != NULL)
-			return (NULL);
-		if (counter == idx)
-			*copyPreviousNode = newNode;
-		else
-			copyPreviousNode = &((*head)->next);
-		(*head) = (*head)->next; /*go to next node*/
-		counter++;
+		if (count == idx - 1)
+		{
+			newNode->next = node->next;
+			node->next = newNode;
+			return (newNode);
+		}
+		count++;
+		node = node->next;
 	}
-
-	/*when we arrive to idxth node*/
-
-	newNode->next = (*head);
-	(*head) = copyHead;
-	return (newNode);
+	free(newNode);
+	return (NULL);
 }
