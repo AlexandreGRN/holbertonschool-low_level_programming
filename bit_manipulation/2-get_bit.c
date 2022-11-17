e2987ac90a0507f9f7d2aae4b770b8e70d605b3c
root <tulkiidra@gmail.com>
2022-11-17 09:19:47 -0800

    upload task 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: number
+ * @index: where is the value
+ * Return: value of the indexth bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (count < index)
+	{
+		count++;
+		if (n > 1)
+		{
+			n = n >> 1;
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (n & 1);
+}
