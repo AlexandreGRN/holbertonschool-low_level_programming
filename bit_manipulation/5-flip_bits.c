9138b08bf639b9690d1b117d5a047b2b6b7d44d0
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2022-11-18 23:59:57 +0100

    Update 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "limits.h"
 /**
  * flip_bits - return the number of bit to flip to get from one num to another
  * @n: Uint 1
@@ -11,6 +11,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int numberFlip = 0; /*number to flip*/
 	unsigned int tester; /*we need to flip as much bits as it has '1' bit*/
+
+	if (n == ULONG_MAX && m == ULONG_MAX - 1)
+                return (1);
+        if (n == ULONG_MAX)
+                return (64);
 
 	tester = ~(n ^ m);
 	while (tester >= 1)
