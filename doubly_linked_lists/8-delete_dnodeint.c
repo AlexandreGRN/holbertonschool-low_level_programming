71483912feb7e7b2b382264414fdf1f82d455cf0
root <tulkiidra@gmail.com>
2022-11-21 09:46:02 -0800

    NULL case

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- * _print_dlistint_backward - print a list but backward
- * @h: head of the list
- * Return: number of elems
- */
-size_t _print_dlistint_backward(const dlistint_t *h)
-{
-	int count = 0;
-
-	while (h->next) /*go end*/
-		h = h->next;
-	while (h->prev) /*print backward + count for lenght*/
-	{
-		count++;
-		printf("%d", h->n); /*print current*/
-		h = h->prev; /*go previous node*/
-	}
-	printf("%d", h->n);
-	return (count);
-}
-/**
  * delete_dnodeint_at_index - delete a node at a given pos
  * @head: head of the list
  * @index: index where to put the node
