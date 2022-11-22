99e7ad67bcbd7f39a5e014f7a89d29dff2bda27f
root <tulkiidra@gmail.com>
2022-11-22 03:23:53 -0800

    many cases task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#define STDERR_FILENO 2
+/**
+ * main - copie the content of a file into another one
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	char *buffer[1024];
+	int fd1, fd2, test1, test2, size = 0;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		return (97);
+	}
+	/*open both files*/
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+	fd2 = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	/*read file 1 + write file2*/
+	while (size = read(fd1, buffer, 1024))
+	{
+		write(fd2, buffer, size);
+	}
+	test1 = close(fd1);
+	if (test1 == 1)
+	{
+		dprintf(2, "Error: Can't close fd %d", test1);
+		return (100);
+	test2 = close(fd2);
+	}
+	if (test1 == 1)
+	{
+		dprintf(2, "Error: Can't close fd %d", test2);
+		return (100);
+	}
+	size = size;
+	return (1);
+	}
