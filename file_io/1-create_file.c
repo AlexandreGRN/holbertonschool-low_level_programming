3b3138049ef073ac421fe57c78a8acbca29c671e
root <tulkiidra@gmail.com>
2022-11-22 00:57:02 -0800

    upload task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - function that create a file
+ * @filename: the file name
+ * @text_content: text to add
+ * Return: 1 or -1 if error
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC, 0600); /*create file*/
+	if (fd == -1)
+		return (-1);
+
+	len = strlen(text_content);
+	write(fd, text_content, len);
+
+	close(fd);
+
+	return (1);
+}
