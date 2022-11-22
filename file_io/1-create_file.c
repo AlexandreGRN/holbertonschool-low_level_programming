416b3e19fc1b4681f0353428658c21f849f71ac0
root <tulkiidra@gmail.com>
2022-11-22 01:01:49 -0800

    if text NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,9 +17,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600); /*create file*/
 	if (fd == -1)
 		return (-1);
-
-	len = strlen(text_content);
-	write(fd, text_content, len);
+	if (text_content)
+	{
+		len = strlen(text_content);
+		write(fd, text_content, len);
+	}
 
 	close(fd);
 
