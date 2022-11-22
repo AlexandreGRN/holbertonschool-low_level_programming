ff6f89f41bf247b2c18946188194e85a734ee504
root <tulkiidra@gmail.com>
2022-11-22 08:51:32 -0800

    Before test function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,67 +1,47 @@
 #include "main.h"
-
 /**
  * main - copie the content of a file into another one
  * @argc: arg count
  * @argv: arg vector
  * Return: 1
  */
-
 int main(int argc, char *argv[])
 {
 	char *buffer[1024];
 	int fd1, fd2, test1 = 0, test2 = 0, test3 = 0, size = 0;
 
-	if (argc == 2)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to ");
-		return (99);
-	}
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
+	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97); }
 	/*open both files*/
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98); }
 	fd2 = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-		return (98);
-	}
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		return (98); }
 	/*read file 1 + write file2*/
 	while ((size = read(fd1, buffer, 1024)))
 	{
 		if (size == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			return (98);
-		}
+		/*{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);*/
+		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			return (99); }
 		test3 = write(fd2, buffer, size);
 		if (test3 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			return (99);
-		}
+		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			return (99); }
 	}
-	test1 = close(fd1);
+	test1 = close(fd1); /*close*/
 	if (test1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", test1);
-		return (100);
-	test2 = close(fd2);
-	}
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", test1);
+		return (100); }
+		test2 = close(fd2);
 	if (test2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", test2);
-		return (100);
-	}
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", test2);
+		return (100); }
 	size = size;
 	return (0);
 }
