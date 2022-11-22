e62cdbe63a38671c791e897e65b1661353ce2fd2
root <tulkiidra@gmail.com>
2022-11-22 01:01:04 -0800

    if text NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600); /*create file*/
 	if (fd == -1)
