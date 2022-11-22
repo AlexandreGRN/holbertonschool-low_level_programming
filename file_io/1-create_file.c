665966fcbf37d3e885be81a572b094e3200c43d7
root <tulkiidra@gmail.com>
2022-11-22 01:00:10 -0800

    changin 0 and O

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL || text_content == NULL)
 		return (-1);
-	fd = open(filename, 0_RDWR | O_CREAT | O_TRUNC, 0600); /*create file*/
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600); /*create file*/
 	if (fd == -1)
 		return (-1);
 
