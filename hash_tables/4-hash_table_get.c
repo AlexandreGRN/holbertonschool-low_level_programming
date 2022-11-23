28a4173ae514f17ae1dc69f2a5103a426c89c9cf
root <tulkiidra@gmail.com>
2022-11-23 10:34:14 -0800

    If ht null

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tempHead;
 
+	if (!ht)
+		return (NULL);
 	/* get index */
 	index = key_index((const unsigned char *)key, ht->size);
 
