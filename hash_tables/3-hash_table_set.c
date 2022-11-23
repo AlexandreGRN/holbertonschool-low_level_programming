da78d23b3046fb32f17a0bd3347724c4831641f3
root <tulkiidra@gmail.com>
2022-11-23 10:15:50 -0800

    little fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			/* replacing the old value in the good spot (corresponding key) */
 			free(tempHead->value);
 			tempHead->value = strdup(value);
-			if (!(newElem->value))
+			if (!(tempHead->value))
 				return (0);
 			return (1);
 		}
