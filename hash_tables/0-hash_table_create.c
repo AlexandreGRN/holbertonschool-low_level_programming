91487334ed7b7f5d81ad89ede0cd4246b0c77f8b
root <tulkiidra@gmail.com>
2022-11-23 02:22:38 -0800

    betty code style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newHash;
 
-	/* create new hash table struc*/
+	/* create new hash_table struc*/
 	newHash = malloc(sizeof(hash_table_t));
 	if (!newHash)
 		return (NULL);
