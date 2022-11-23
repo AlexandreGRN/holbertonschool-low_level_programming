e9f315240c7b1bf0a6f5b0a70013319336905d0c
root <tulkiidra@gmail.com>
2022-11-23 02:19:49 -0800

    go back

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,10 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *newHash;
 
 	/* create new hash table struc*/
-	if (newHash)
-		newHash = realloc(sizeof(hash_table_t));
-	else
-		newHash = malloc(sizeof(hash_table_t));
+	newHash = malloc(sizeof(hash_table_t));
 	newHash->size = size;
 	newHash->array = malloc(sizeof(hash_node_t));
 
