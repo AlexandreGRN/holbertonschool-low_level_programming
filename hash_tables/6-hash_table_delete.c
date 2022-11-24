e3fec726ae8515c03728891c967bfc4f7767771c
root <tulkiidra@gmail.com>
2022-11-24 04:54:34 -0800

    Try

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - print all the hash table
+ * @ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int count;
+	hash_node_t *currentHead;
+	hash_node_t *nextNode;
+
+	for (count = 0; count < ht->size ; count++)
+	{
+		currentHead = ht->array[count];
+		while (currentHead)
+		{
+			nextNode = currentHead->next;
+			free(currentHead);
+			currentHead = nextNode;
+		}
+	}
+	free(currentHead);
+	free(ht);
+	free(nextNode);
+}
